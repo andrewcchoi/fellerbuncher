f93a4e72a386c6ea0ff2468507e3350c03271880
andrew choi <74335726+andrewcchoi@users.noreply.github.com>
2025-04-21 11:47:22 -0700

    combine config into python file.

---
+++ main_fellerbuncher2.py
@@ -0,0 +1,28 @@
+import atexit
+import json
+import logging.config
+import logging.handlers
+import pathlib
+
+from fellerbuncher2 import Fellerbuncher
+
+lumberjack = logging.getLogger(__name__)
+
+def main():
+    # setup_logging()
+    Fellerbuncher()
+    logging.basicConfig(level="INFO")
+    lumberjack.debug("debug message", extra={"acdc": "sample line"})
+    lumberjack.info("info message")
+    lumberjack.warning("warning message")
+    lumberjack.error("error message")
+    lumberjack.critical("critical message")
+
+    try:
+        1 / 0
+    except ZeroDivisionError:
+        lumberjack.exception("exception message:\n")
+
+
+if __name__ == "__main__":
+    main()
