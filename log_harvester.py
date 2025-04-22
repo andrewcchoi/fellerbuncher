07f403722ca5a42ded0f9ed0e22c76d7bb3d05ed
andrew choi <74335726+andrewcchoi@users.noreply.github.com>
2025-04-21 21:53:32 -0700

    refactored file.

---
+++ log_harvester.py
@@ -0,0 +1,148 @@
+import atexit
+import json
+import logging.config
+import logging.handlers
+import pathlib
+
+import _config as config
+
+def logHarvestConfiguration(
+    host: str, 
+    port: str, 
+    sender: str, 
+    to: str, 
+    user: str, 
+    pwd: str,
+    timeout: int = 5
+) -> json:
+  """Configure logging for the Fellerbuncher application."""
+
+  harvestConfig = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+      "simple": {
+        "format": "%(asctime)s: %(message)s",
+        "datefmt": "%Y-%m-%dT%H:%M:%S%z"
+      },
+      "detailed": {
+        "format": "%(asctime)s [%(levelname)s|%(module)s|L%(lineno)d]: %(message)s",
+        "datefmt": "%Y-%m-%dT%H:%M:%S%z"
+      },
+      "json": {
+        "()": "log_formatter.HarvestJSONFormatter",
+        "fmt_keys": {
+          "timestamp": "timestamp",
+          "level": "levelname",
+          "message": "message",
+          "logger": "name",
+          "module": "module",
+          "function": "funcName",
+          "line": "lineno",
+          "thread_name": "threadName"
+        }
+      }
+    },
+    "filters": {
+      "no_errors": {
+        "()": "log_formatter.NonErrorFilter"
+      }
+    },
+    "handlers": {
+      "stdout": {
+        "class": "logging.StreamHandler",
+        "formatter": "simple",
+        "stream": "ext://sys.stdout",
+        "filters": ["no_errors"]
+      },
+      "stderr": {
+        "class": "logging.StreamHandler",
+        "level": "WARNING",
+        "formatter": "detailed",
+        "stream": "ext://sys.stderr"
+      },
+      "fellerbuncher_json": {
+        "class": "logging.handlers.RotatingFileHandler",
+        "level": "DEBUG",
+        "formatter": "json",
+        "filename": "logs/fellerbuncher.log.jsonl",
+        "maxBytes": 10000,
+        "backupCount": 3
+      },
+      "fellerbuncher_mail": {
+        "class": "logging.handlers.SMTPHandler",
+        "level": "ERROR",
+        "formatter": "json",
+        "mailhost": (host,port),
+        "fromaddr": sender,
+        "toaddrs": to,
+        "subject": "Fellerbuncher Error",
+        "credentials": (user,rf'{pwd}'),
+        "secure": (),
+        "timeout": timeout
+      },
+      "queue_handler": {
+        "class": "logging.handlers.QueueHandler",
+        "handlers": [
+          "stdout",
+          "stderr",
+          "fellerbuncher_json",
+          "fellerbuncher_mail"
+        ],
+        "respect_handler_level": True
+      }
+    },
+    "loggers": {
+      "root": {
+        "level": "DEBUG",
+        "handlers": [
+          "queue_handler"
+        ]
+      }
+    }
+  }
+  return harvestConfig
+
+
+def Fellerbuncher() -> None:
+  """Initialize the Fellerbuncher application."""
+  
+  # Configure logging
+  harvestConfig = logHarvestConfiguration(
+    host=config.EMAIL_HOST, 
+    port=config.EMAIL_PORT[1], 
+    sender=config.EMAIL_SENDER, 
+    to=config.EMAIL_TO, 
+    user=config.EMAIL_USER, 
+    pwd=config.EMAIL_PWD,
+    timeout=1
+  )
+
+  # Create logs directory if it doesn't exist
+  logs_dir = pathlib.Path("logs")
+  logs_dir.mkdir(parents=True, exist_ok=True)
+
+  logging.config.dictConfig(harvestConfig)
+  queue_handler = logging.getHandlerByName("queue_handler")
+  if queue_handler is not None:
+    queue_handler.listener.start()
+    atexit.register(queue_handler.listener.stop)
+
+
+if __name__ == "__main__":
+  
+  lumberjack = logging.getLogger(__name__)
+  
+  Fellerbuncher()
+  logging.basicConfig(level="DEBUG")
+
+  lumberjack.info("Fellerbuncher started", extra={"acdc": "EXTRA INFO"})
+  lumberjack.debug("Debugging information", extra={"acdc": "EXTRA DEBUG"})
+  lumberjack.warning("Warning message", extra={"acdc": "EXTRA WARNING"})
+  lumberjack.error("Error message", extra={"acdc": "EXTRA ERROR"})
+  lumberjack.critical("Critical error message", extra={"acdc": "EXTRA CRITICAL"})
+
+  try:
+      1 / 0
+  except ZeroDivisionError:
+      lumberjack.exception("exception message:\n")
